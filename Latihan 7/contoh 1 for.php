80e0bce7c692970ef5edb32a209f512687f42d30
Puputera <127200098+Puputera@users.noreply.github.com>
2023-05-12 08:13:03 +0700

    latihnan 7

---
+++ Latihan 7/contoh 1 for.php
@@ -0,0 +1,4 @@
+<?php
+    for ($x=1;$x<=10;$x++) /* Nilai Awal, Akhir, Step */
+    {echo $x; /*Statement*/}
+?>
---
+++ Latihan 7/contoh 2 while.php
@@ -0,0 +1,23 @@
+<?php
+    $i=1;
+    while($i<=10) {
+    echo $i. "<br>";
+    $i=$i+1;
+    }
+?>
+<br><br>
+<?php
+    $i=2;
+    while($i<=8){
+        echo $i."<br>";
+        $i=$i*2;
+    }
+?>
+<br><br>
+<?php
+    $i=2;
+    while($i<=16){
+        echo $i."<br>";
+        $i=$i*$i;
+    }
+?>
