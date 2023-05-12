48b40f4b53faeb96283ba55ff0d60a3d6ac74a30
Puputera <127200098+Puputera@users.noreply.github.com>
2023-05-12 08:13:28 +0700

    tugas 5

---
+++ Lembar Tugas KP5a.php
@@ -0,0 +1,15 @@
+<?php
+    $nama = "Andi Firmansyah";
+    $lahir = "Cilacap, 26 April 1991";
+    $alamat = "Jl. Kalimantan, Cilacap";
+    $agama = "Islam";
+    $pekerjaan = "Mahasiswa";
+    $status = "Perjaka";
+
+    echo "BIODATA SAYA ADALAH SEBAGAI BERIKUT";
+    echo "Nama Lengkap : $nama <br>";
+    echo "Tempat/Tanggal Lahir :  $lahir <br>";
+    echo "Alamat :  $alamat <br>";
+    echo "Agama :  $agama <br>";
+    echo "Pekerjaan :  $pekerjaan <br>";
+?>
