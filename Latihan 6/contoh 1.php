b64835636a9687f1ffd3170c473f89ccbe72edc4
Puputera <127200098+Puputera@users.noreply.github.com>
2023-05-12 08:12:16 +0700

    latihan 6

---
+++ Latihan 6/contoh 1.php
@@ -0,0 +1,36 @@
+<?php
+    echo "penjumlahan : <br>";
+    $x =5;
+    $y =3;
+    $z = $x + $y;
+    //Menampilkan Hasil
+    echo "hasil = $z <br>";
+
+    echo "pengurang : <br>";
+    $a = 5;
+    $b = 3;
+    $c = $a - $b;
+    //Menampilkan Hasil
+    echo "hasil = $c <br>";
+
+    echo "perkalian : <br>";
+    $d =5;
+    $e =3;
+    $f = $d * $e;
+    //Menampilkan Hasil
+    echo "hasil = $f <br>"; 
+
+    echo "pembagian : <br>";
+    $k =10;
+    $l =2;
+    $m = $k / $l;
+    //Menampilkan Hasil
+    echo "hasil = $m <br>";
+
+    echo "modulus : <br>";
+    $p =5;
+    $q =3;
+    $r = $p % $q;
+    //Menampilkan Hasil
+    echo "hasil = $r <br>";
+?>
